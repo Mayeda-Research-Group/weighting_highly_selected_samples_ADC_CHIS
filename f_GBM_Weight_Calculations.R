dfa9955bc60ec43a95f2d536b0d7f22a76063481
Elizabeth Rose Mayeda <ermayeda@ph.ucla.edu>
2022-01-31 11:05:20 -0800

    Jan 2022 version of GBM code
    Education is now a factor variable and reference levels for education, race, and language manually selected. Code updated to specify using legacy version of GBM package.

--- f_GBM_Weight_Calculations.R
+++ f_GBM_Weight_Calculations.R
@@ -1,35 +1,34 @@
-# GBM
-# Sept. 22, 2019
+# Reweighting for Highly Selected Samples
+# Gradient Boosted Models (GBM)
+# Kristina Van Dang
+# January 26, 2022
+# Script name "GBM_26Jan2022.R"
+
+# This code produces the predicted probabilities of being in CHIS using GBM
 
 rm(list=ls())
-library(twang)
-library(gbm)
 
-setwd("/Users/kristinadang/Box Sync/UCD Health Cognition/Data/ADC-CHIS_DATA")
+library(twang) # GBM; twang_2.5
+library(gbm) # gbm_2.1.8
+
+setwd("C:/Users/ehlarson/Box/KD_bootstrapping/hoffman_cluster/data")
+
 df1<-read.csv("adc_chis2_am_09.csv") 
 
-# Data management
-#------------------
-# Leave education, race, and language as categorical
-# Check about ordered/factor Edu_harm1, 
-# https://datascience.stackexchange.com/questions/14025/ordinal-feature-in-decision-tree
-# Here is the factor version:
-# df1$Edu_harm1_cat<-factor(df1$Edu_harm1)
-
+# ---- Data management -----
 # The twang package (page 3) had recommended to use
 # categorical variables stored as factors or ordered,
-# Here is the ordered version (note the structure of Edu_harm1_cat vs. Edu_harm1)
-# Decided on Apr 6, 2019 that according to Twang docu and website, to keep it as ordered
-df1$Edu_harm1<-ordered(df1$Edu_harm1)
-df1$race<-factor(df1$race)
-df1$language1<-factor(df1$language1)
+
+#EHL updating code to make race, education, and language factors with uniform references across algorithms
+df1$Edu_harm1<-factor(df1$Edu_harm1, levels=c(3,0,1,2,4,5))
+df1$race<-factor(df1$race, levels=c(3,1,2))
+df1$language1<-factor(df1$language1, levels=c(1,2,3))
 
 # Chis as the outcome variable
 # We can take the complement of this after the gbm to get the pp of ADC
 df1$chis<-ifelse(df1$adc==1,0,1)
 
-# GBM run
-#------------
+# ---- GBM -----
 # Here we have 4 different runs (each time with the corresponding interaction depth)
 # We are asking GBM to give us the predicted probability of chis based on all those covs
 # 10,000 trees, shrinkage rate of 0.01, es.mean stop method, ATT estimand, 
@@ -43,7 +42,7 @@ fit1.ps <- ps(chis~race+male+Nage+diabetes+stroke+BMI+Cholesterol+HeartDz+Conges
               shrinkage = 0.01,
               stop.method="es.mean",
               estimand = "ATT",
-              sampw = df1$RAKEDW0)
+              sampw = df1$RAKEDW0, version = "legacy")
 
 set.seed(140111)
 fit2.ps <- ps(chis~race+male+Nage+diabetes+stroke+BMI+Cholesterol+HeartDz+CongestiveHeart+
@@ -54,7 +53,7 @@ fit2.ps <- ps(chis~race+male+Nage+diabetes+stroke+BMI+Cholesterol+HeartDz+Conges
               shrinkage = 0.01,
               stop.method="es.mean",
               estimand = "ATT",
-              sampw = df1$RAKEDW0)
+              sampw = df1$RAKEDW0, version = "legacy")
 
 set.seed(140111)
 fit3.ps <- ps(chis~race+male+Nage+diabetes+stroke+BMI+Cholesterol+HeartDz+CongestiveHeart+
@@ -65,7 +64,7 @@ fit3.ps <- ps(chis~race+male+Nage+diabetes+stroke+BMI+Cholesterol+HeartDz+Conges
               shrinkage = 0.01,
               stop.method="es.mean",
               estimand = "ATT",
-              sampw = df1$RAKEDW0)
+              sampw = df1$RAKEDW0, version = "legacy")
 
 set.seed(140111)
 fit4.ps <- ps(chis~race+male+Nage+diabetes+stroke+BMI+Cholesterol+HeartDz+CongestiveHeart+
@@ -76,7 +75,7 @@ fit4.ps <- ps(chis~race+male+Nage+diabetes+stroke+BMI+Cholesterol+HeartDz+Conges
               shrinkage = 0.01,
               stop.method="es.mean",
               estimand = "ATT",
-              sampw = df1$RAKEDW0)
+              sampw = df1$RAKEDW0, version = "legacy")
 
 
 
@@ -97,21 +96,4 @@ df1$pp4_CHIS <- fit4.ps$ps[["es.mean.ATT"]]
 df1$pp4_ADC <- 1 - df1$pp4_CHIS
 
 
-write.csv(df1, file="GBMpp_09_092219.csv")
-
-# Check about ordered/factor Edu_harm1
-# set.seed(140111)
-# fit1.ps_check <- ps(chis~race+male+Nage+diabetes+stroke+BMI+Cholesterol+HeartDz+CongestiveHeart+
-#                 Edu_harm1_cat+language1+marry_bi,
-#               data = df1, 
-#               n.trees = 10000,
-#               interaction.depth = 1, 
-#               shrinkage = 0.01,
-#               stop.method="es.mean",
-#               estimand = "ATT",
-#               sampw = df1$RAKEDW0)
-# 
-# df1$pp1_CHIS_check <- fit1.ps_check$ps[["es.mean.ATT"]]
-# df1$pp1_ADC_check <- 1 - df1$pp1_CHIS_check
-
-
+write.csv(df1, file="C:/Users/ehlarson/Box/KD_bootstrapping/Scripts_26Jan2022/results/GBM_09_26Jan2022.csv")
