44209c02e487452b9d248708c623edbc82fb86b5
Elizabeth Rose Mayeda <ermayeda@ph.ucla.edu>
2022-07-25 15:00:41 -0700

    Add files via upload

---
+++ submission_scripts/submit_KD_bootstrap_missing_seeds.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#$ -cwd #uses current working directory
+# error = Merged with joblog
+#$ -o joblogs/joblog.$JOB_ID.$TASK_ID #creates a file called joblog.jobidnumber to write to. 
+#$ -j y 
+#$ -l h_rt=4:00:00,h_data=4G,arch=intel-E5-*|intel-gold-* #requests 4 hours, 4GB of data (per core), specific intel nodes (required for twang package)
+#$ -pe shared 1 #requests 1 core
+# Email address to notify
+#$ -M $USER@mail #don't change this line, finds your email in the system 
+# Notify when
+#$ -m bea #sends you an email (b) when the job begins (e) when job ends (a) when job is aborted (error)
+# submit array job:
+# TEST RUN:
+##$ -t 1-3:1
+# FULL RUN (length of the array of missing jobs):
+#$ -t 1-11:1
+## 
+
+# load the job environment:
+. /u/local/Modules/default/init/modules.sh
+module load R/4.0.2 #loads R/4.0.2 for use 
+module load gcc/10.2.0 #loads gcc which is necessary for twang's xgboost dependency
+export OMP_NUM_THREADS=1 #uses max 1 threads (needs to match -pe shared)
+## 
+# run R code
+
+echo "======"
+echo SGE_TASK_ID=$SGE_TASK_ID  
+
+# SET THE ARRAY WITH THE $SGE_TASK_IDs that failed 
+### (note: UPDATE line "#$ -t 1-2000:1" ACCORDINGLY!!!) 
+
+array=(223 511 531 564 740 748 1042 1324 1363 1633 1680)
+
+# SET THE SEED
+###(note: array index starts at 0 so we need to scale back the current $SGE_TASK_ID)
+myindex=${array[$SGE_TASK_ID-1]}
+
+R CMD BATCH --no-save --no-restore "--args seed=$myindex "  hoffman_Analysis_18May2022.R output/output.$JOB_ID.$SGE_TASK_ID
+echo R CMD BATCH --no-save --no-restore  hoffman_Analysis_18May2022.R output/output.$JOB_ID.$SGE_TASK_ID
+
+
